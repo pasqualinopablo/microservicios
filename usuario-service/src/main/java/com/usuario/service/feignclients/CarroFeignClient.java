ecffc7ea731de1879d9a654351483364275c65ba
pasqualinopablo <pasqualinopablo@gmail.com>
2023-04-24 13:18:17 -0300

    fix puerto del gateway en los Feign Clients

--- usuario-service/src/main/java/com/usuario/service/feignclients/CarroFeignClient.java
+++ usuario-service/src/main/java/com/usuario/service/feignclients/CarroFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name= "carro-service", url="http://localhost:8002")
+@FeignClient(name= "carro-service", url="http://localhost:8080")
 public interface CarroFeignClient {
 
     @PostMapping("/carro")
--- usuario-service/src/main/java/com/usuario/service/feignclients/MotoFeignClient.java
+++ usuario-service/src/main/java/com/usuario/service/feignclients/MotoFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name= "moto-service", url="http://localhost:8003")
+@FeignClient(name= "moto-service", url="http://localhost:8080")
 public interface MotoFeignClient {
 
     @PostMapping("/moto")
